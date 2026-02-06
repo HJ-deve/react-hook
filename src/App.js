84f29c0815971420b892950b502e0a9e5c24b331
HJ <hunjoolee@HUNJOO-M4-MacBook-Pro.local>
2026-02-06 19:34:20 +0900

    react-hook-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useInput } from "./useInput.js";
+
+function displayMessage(message) {
+  alert(message);
+}
 
 function App() {
+  const [inputValue, handleChange, handleSubmit] = useInput("", displayMessage);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>useInput</h1>
+      <input value={inputValue} onChange={handleChange} />
+
+      <button onClick={handleSubmit}>확인</button>
     </div>
   );
 }
